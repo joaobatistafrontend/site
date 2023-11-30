a412bc1035df30294b29b15761b48a221b3a5be5
joao batista <jbbuno007@gmail.com>
2023-11-30 12:17:23 -0300

    tiando o request

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import requests
 from django.views.generic import View
 from django.http import JsonResponse
 class Index(View):
